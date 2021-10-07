dabe6676e09afa87583dddec3f1c354463f2c8ae
Gerardo Zinno <gerardozinno1@gmail.com>
2021-10-07 19:45:22 +0200

    Fixed class redefinition.

--- src/utimer.cpp
+++ src/utimer.cpp
@@ -6,36 +6,23 @@
     auto elapsed =                                                                                                     \
         std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - timename).count();
 
-class UTimer
+UTimer::UTimer(const std::string m) : message(m), us_elapsed((long *)NULL)
 {
-    std::chrono::system_clock::time_point start;
-    std::chrono::system_clock::time_point stop;
-    std::string message;
-    using usecs = std::chrono::microseconds;
-    using msecs = std::chrono::milliseconds;
+    start = std::chrono::system_clock::now();
+}
 
-  protected:
-    long *us_elapsed;
+UTimer::UTimer(const std::string m, long *us) : message(m), us_elapsed(us)
+{
+    start = std::chrono::system_clock::now();
+}
 
-  public:
-    UTimer(const std::string m) : message(m), us_elapsed((long *)NULL)
-    {
-        start = std::chrono::system_clock::now();
-    }
+UTimer::~UTimer()
+{
+    stop = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = stop - start;
+    auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
 
-    UTimer(const std::string m, long *us) : message(m), us_elapsed(us)
-    {
-        start = std::chrono::system_clock::now();
-    }
-
-    ~UTimer()
-    {
-        stop = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed = stop - start;
-        auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-
-        std::cout << message << " computed in " << musec << " usec " << std::endl;
-        if (us_elapsed != NULL)
-            (*us_elapsed) = musec;
-    }
-};
+    std::cout << message << " computed in " << musec << " usec " << std::endl;
+    if (us_elapsed != NULL)
+        (*us_elapsed) = musec;
+}
