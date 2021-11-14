2b0e65115c50f5876044273aebd6b902a11fb388
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-11-14 18:36:58 +0100

    Add valgrind test

--- tests/unit/test_sequential_leaks_valgrind.cpp
+++ tests/unit/test_sequential_leaks_valgrind.cpp
@@ -13,22 +13,10 @@ using namespace ca;
 int main()
 {
     {
-        const size_t rows = 4;
-        const size_t columns = 5;
-        int grid[rows][columns] = {{0, -2, 0, 0, -1}, {0, 1, 2, 3, -2}, {0, 4, 0, 5, 0}, {9, 6, 7, 8, 0}};
-        auto update_fn = [](int x, int a, int b, int c, int d, int e, int f, int g, int h) {
-            return x + a + b + c + d + e + f + g + h;
-        };
-        auto c = seq::CellularAutomaton<int>(grid, rows, columns, update_fn);
-        c.simulate(5);
-        c.get_generation();
-    }
-
-    {
         const size_t rows = 100;
         const size_t columns = 100;
         double **grid = new double *[rows];
-        for (int i = 0; i < rows; ++i)
+        for (size_t i{0}; i < rows; ++i)
         {
             grid[i] = new double[columns];
         }
@@ -40,7 +28,7 @@ int main()
         c.simulate(0);
         c.simulate(1);
 
-        for (int i = 0; i < rows; ++i)
+        for (size_t i{0}; i < rows; ++i)
         {
             delete[] grid[i];
         }
