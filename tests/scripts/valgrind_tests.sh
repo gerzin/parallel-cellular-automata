d684530701868c4a5a9fc172b66194a109aa5d5b
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-11-16 13:25:10 +0100

    Add quiet mode to valgrind

--- tests/scripts/valgrind_tests.sh
+++ tests/scripts/valgrind_tests.sh
@@ -21,7 +21,7 @@ echo "RUNNING THE TEST FILES WITH valgrind"
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 for file in $files; do
-    exitcode=$(valgrind -q --leak-check=full --error-exitcode=2 "./$file" >/dev/null)
+    exitcode=$(valgrind -q --leak-check=full --error-exitcode=2 "./$file")
     if [ "$exitcode" -eq 2 ]; then
         echo "${RED}LEAKS DETECTED IN: $file ${NC}"
         echo "EXITING"
