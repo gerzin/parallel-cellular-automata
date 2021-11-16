740520ade36feeaa9887529e711392d745401aea
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-11-16 13:16:19 +0100

    Echo errors in red

--- tests/scripts/valgrind_tests.sh
+++ tests/scripts/valgrind_tests.sh
@@ -17,14 +17,19 @@ files=$(make help | grep valgrind | grep -v unit | cut -c 5-)
 echo "TEST_FILES: $files"
 echo "RUNNING make"
 make "$files"
-
+echo "RUNNING THE TEST FILES WITH valgrind"
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 for file in $files; do
-    exitcode=$(valgrind --leak-check=full --error-exitcode=2 "./$file")
+    exitcode=$(valgrind --leak-check=full --error-exitcode=2 "./$file" >/dev/null)
     if [ "$exitcode" -eq 2 ]; then
-        echo "LEAKS DETECTED IN: $file"
+        echo "${RED}LEAKS DETECTED IN: $file ${NC}"
         echo "EXITING"
         exit 1;
+    else
+        echo "NO LEAKS DETECTED IN: $file"
     fi
+
 done
 
 echo "NO LEAKS DETECTED"
