16dc51169b50653c6804f09b928f6a76ff1d5d66
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2021-12-23 12:40:39 +0100

    Add ff_automaton file

---
+++ include/ff_automaton.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file ff_automaton.hpp
+ * @author Gerardo Zinno (gerardozinno1@gmail.com)
+ * @brief This file contains the definition and implementation of the parallel version of the Cellular Automaton.
+ *
+ *        This version is parallelized using the fastflow framework.
+ *
+ * @version 0.1
+ * @date 2021-12-22
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef PARALLEL_CELLULAR_AUTOMATA_FASTFLOW_AUTOMATON_HPP
+#define PARALLEL_CELLULAR_AUTOMATA_FASTFLOW_AUTOMATON_HPP
+
+#endif
