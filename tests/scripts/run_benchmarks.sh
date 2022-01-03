db4d8f2d63c9e23ab8e3a120a60037bdfe1f0d8a
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-01-03 13:48:33 +0100

    Add script to run the benchmarks

---
+++ tests/scripts/run_benchmarks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "This script will compile, run and save all the benchmarks"
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) ;;
+  n|N ) exit 1;;
+  * ) echo "invalid"; exit 1;;
+esac
+
+echo "Moving into benchmark/build"
+cd ../benchmarks
+if [ ! -d build ]; then
+    mkdir build
+    cd build
+else
+    cd build
+    rm -rf *
+fi
+echo "Compiling the benchmarks"
+cmake ..
+make
+
+BENCHMARKS=$(ls | grep bench)
+
+for item in $BENCHMARKS; do
+    ./$item --benchmark_out_format=json --benchmark_out=../outputs/$item.$USERNAME.$HOSTNAME.json
+done
